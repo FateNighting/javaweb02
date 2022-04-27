92e010598816b9af0aa28c39126fcab0b84c6555
zhangpeng <1547143416@qq.com>
2022-04-27 23:43:48 +0800

    Initial commit
    Signed-off-by: zhangpeng <1547143416@qq.com>

--- cookie/src/main/java/com/zhang/test/Test1.java
+++ cookie/src/main/java/com/zhang/test/Test1.java
@@ -5,4 +5,8 @@ package com.zhang.test;
  * @create 2022-04-27  20:45
  */
 public class Test1 {
+    private static final String zhangpeng ="章鹏";
+    public static void main(String[] args) {
+        System.out.println(zhangpeng);
+    }
 }
