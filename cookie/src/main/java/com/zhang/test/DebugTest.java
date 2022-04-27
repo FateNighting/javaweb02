95efb81c67ae0d52246140bea3d9a0c65ab21625
zhangpeng <1547143416@qq.com>
2022-04-28 00:01:31 +0800

    Initial commit
    Signed-off-by: zhangpeng <1547143416@qq.com>

---
+++ cookie/src/main/java/com/zhang/test/DebugTest.java
@@ -0,0 +1,25 @@
+package com.zhang.test;
+
+import java.util.HashMap;
+
+/**
+ * @author 章鹏
+ * @create 2022-04-27  23:52
+ */
+public class DebugTest {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        map.put("name","tom");
+        map.put("age","12");
+        map.put("school","Tsinghua");
+        map.put("major","computer");
+
+            String age=map.get("age");
+        System.out.println(age);
+
+        map.remove("major");
+        System.out.println(map);
+    }
+}
